a3307c70185200d0760dd6cad800ceca1fb65bc5
CamilosolerB <caansobu2@gmail.com>
2021-10-15 21:51:15 -0500

    modificacion database

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,8 +1,9 @@
 const mysql =require('mysql')//habilita para usar mysql
 module.exports=()=>//hace la exportacion de datos
 mysql.createConnection({//trae los datos importantes
-    host:'localhost',
-    user:'root',
-    password: '',
-    database:'banco'
+    host:'en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user:'bf6f59xnxbjjmngr',
+    password: 'ol0iqr1jzgu5zu66',
+    database:'getu19rdqt8dk5nt',
+    port:"3306"
 })
