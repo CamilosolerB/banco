3588b9cce98f54232b03cda4411f4f0ac3165164
unknown <caansobu2@gmail.com>
2021-11-23 17:41:50 -0500

    despliegue a heroku

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,9 +1,9 @@
 const mysql =require('mysql')//habilita para usar mysql
 module.exports=()=>//hace la exportacion de datos
 mysql.createConnection({//trae los datos importantes
-    host:'en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user:'bf6f59xnxbjjmngr',
-    password: 'ol0iqr1jzgu5zu66',
-    database:'getu19rdqt8dk5nt',
+    host:'b4wvpgbs0pfdzjvlqp7r-mysql.services.clever-cloud.com',
+    user:'uy0dnfdgmkd1uctz',
+    password: 'EQ9OIHZNY1UYe1Nl87uk',
+    database:'b4wvpgbs0pfdzjvlqp7r',
     port:"3306"
 })
